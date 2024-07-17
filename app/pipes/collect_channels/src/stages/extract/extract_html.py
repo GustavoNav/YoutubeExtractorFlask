61ae501979858751ee2ac96ec47f462336a185f1
Gustavo Gusta <gustavo.sempre.estude@gmail.com>
2024-07-17 17:25:03 -0300

    bug fix: removing json from cache

--- app/pipes/collect_channels/src/stages/extract/extract_html.py
+++
@@ -1,8 +0,0 @@
-from app.pipes.collect_channels.src.drivers.http_requester import HttpRequester
-
-class ExtractHtml:
-    def __init__(self) -> None:
-        self.__http_requester = HttpRequester()
-
-    def extract(self, urls):
-        self.__http_requester.request(urls)
